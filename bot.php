83daf0662257661b78442a3a1ffa1e018a8a7bb5
zoadyik <grinzsolo@stephack.com>
2017-05-23 13:15:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,11 +17,12 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if($text == '1'){
 			$messages = [
 				'type' => 'text',
 				'text' => '123131313'
 			];
-
+			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
