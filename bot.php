2b276f245a182b37e5a7384eb5016e4dc9907e69
zoadyik <grinzsolo@stephack.com>
2017-05-23 10:33:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,3 +2,4 @@
 $ composer require linecorp/line-bot-sdk
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('pXycMNZQmqDQMsRNxAiOHply1ABxJY6tS+9r4JItT3fuiQUEkZK5goIbdmxhNSngg2KhJAuhh5A59bzTGBkvl1yLy7Im7VXmWfZTUoQUOjoYQPOQyH6Q9ae4ViTrxCOYtWdNxO2/zC1lp4YTiERB7QdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '7fe8857acdcda6ae410ec97ac4d04410']);
+$response = $bot->replyText('<reply token>', 'hello!');
